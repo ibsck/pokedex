a3149e79106dff3cb17b81efc7e16d49ed5041e9
ibsck <ibrahimcasscou19@gmail.com>
2023-05-14 18:22:46 +0200

    format date et survol color

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,23 +1,35 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent,useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
   
 type Props = {
   pokemon: Pokemon
+  borderColor?: string
 };
   
-const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
+    const [color, setColor] = useState<string>('');
+    const showBorder = () => {
+        setColor(borderColor);
+    }
+    const hideBorder = () => {
+        setColor('#f5f5f5');
+    }
+
+    const formatDate = (date: Date): string => {
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    }
     
   return (
-    <div className="col s6 m4">
-      <div className="card horizontal">
+    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+      <div className="card horizontal" style = {{borderColor : color}}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
         <div className="card-stacked">
           <div className="card-content">
             <p>{pokemon.name}</p>
-            <p><small>{pokemon.created.toString()}</small></p>
+            <p><small>{formatDate(pokemon.created)}</small></p>
           </div>
         </div>
       </div> 
