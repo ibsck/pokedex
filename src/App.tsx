3b45e642a82b44bb1eb67249b261f850dd0c4346
ibsck <ibrahimcasscou19@gmail.com>
2023-05-17 10:59:49 +0200

    ajout des routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,29 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon';
 import PokemonList from './pages/pokemon-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App: FunctionComponent = () => {
     return (
-        <PokemonList />
+        <Router>
+            <div>
+                { /*la barre de navigation*/ }
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <a href="/" className="brand-logo center">Pokédex</a>
+                    </div>
+                </nav>
+
+                { /*le contenu de la page*/ }
+                <Switch>
+                    <Route exact path="/" component={PokemonList} />
+                    <Route exact path="/pokemons" component={PokemonList} />
+                    <Route exact path="/pokemons/:id" component={PokemonsDetail} />
+                </Switch>
+            </div>
+                    
+        </Router>
     )
 }
 
--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -1,14 +1,17 @@
-import React, {FunctionComponent,useState, useEffect} from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import Pokemon from '../models/pokemon';
 import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
 
-const PokemonList:FunctionComponent = () => {
+
+const PokemonList: FunctionComponent = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    
 
     useEffect(() => {
         setPokemons(POKEMONS);
     }, []);
+
 
     return (
         <div>
@@ -16,9 +19,9 @@ const PokemonList:FunctionComponent = () => {
             <div className="container">
                 <div className="row">
                     {pokemons.map(pokemon => (
-                        
-                            <PokemonCard key={pokemon.id} pokemon={pokemon} />  
-                                                ))}
+
+                        <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                    ))}
                 </div>
             </div>
         </div>
